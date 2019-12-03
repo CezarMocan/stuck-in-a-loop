e8f51c05b62168bcf7a9ecc10b1ea51e0b1682d5
Cezar Mocan <mocancezar@gmail.com>
2019-12-03 11:38:10 -0500

    Instance management

--- src/InstanceStateManager.cpp
+++ src/InstanceStateManager.cpp
@@ -6,3 +6,27 @@
 //
 
 #include "InstanceStateManager.h"
+
+InstanceStateManager::InstanceStateManager() {
+    installationState = IDLE;
+    phoneState = DOWN;
+    lightState = OFF;
+    characterState = ABSENT;
+    
+//    videoPaths[0]
+}
+
+ofVideoPlayer InstanceStateManager::getVideoForCurrentState() {
+    
+}
+
+void InstanceStateManager::updateState(INSTALLATION_STATE i, PHONE_STATE p, LIGHT_STATE l, CHARACTER_STATE c) {
+    
+    
+    installationState = i;
+    phoneState = p;
+    lightState = l;
+    characterState = c;
+}
+
+
--- src/InstanceStateManager.h
+++ src/InstanceStateManager.h
@@ -5,24 +5,35 @@
 //  Created by Cezar Mocan on 12/3/19.
 //
 
-#ifndef StateManager_hpp
-#define StateManager_hpp
+#ifndef InstanceStateManager_h
+#define InstanceStateManager_h
 
 #include <stdio.h>
+#include "ofMain.h"
 
 enum INSTALLATION_STATE { IDLE, JANE_CALLING, HAPPY, NEUTRAL, ANGRY, FRUSTRATED, RESIGNED };
 enum PHONE_STATE { DOWN, RINGING, UP };
-enum LIGHT_STATE { ON, OFF };
+enum LIGHT_STATE { OFF, ON };
 enum CHARACTER_STATE { ABSENT, PRESENT, WALK_IN, WALK_OUT };
 
 class InstanceStateManager {
     public:
+        InstanceStateManager();
+        ofVideoPlayer getVideoForCurrentState();
+        void updateState(INSTALLATION_STATE i, PHONE_STATE p, LIGHT_STATE l, CHARACTER_STATE c);
         
     private:
         INSTALLATION_STATE installationState;
         PHONE_STATE phoneState;
         LIGHT_STATE lightState;
         CHARACTER_STATE characterState;
+    
+        // [clientId][INSTALLATION_STATE][PHONE_STATE][LIGHT_STATE][CHARACTER_STATE]
+        string[4][8][5][5][5] videoPaths;
+    
+        // [INSTALLATION_STATE][PHONE_STATE][LIGHT_STATE][CHARACTER_STATE]
+        ofVideoPlayer[8][5][5][5] videos;
+        
 };
 
-#endif /* StateManager_hpp */
+#endif /* InstanceStateManager_h */
